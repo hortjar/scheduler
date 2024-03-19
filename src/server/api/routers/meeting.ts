20de6070348980e1ba8c5cde05184276e572149b
Jaroslav Hort <hortjaroslav0@gmail.com>
2024-03-19 07:44:37 +0100

    feat: add trpc

---
+++ src/server/api/routers/meeting.ts
@@ -0,0 +1,27 @@
+import { createId } from "@paralleldrive/cuid2";
+import { eq } from "drizzle-orm";
+import { z } from "zod";
+
+import {
+  createTRPCRouter,
+  protectedProcedure,
+  publicProcedure,
+} from "~/server/api/trpc";
+import { meetings } from "~/server/db/schema";
+
+export const meetingRouter = createTRPCRouter({
+  get: publicProcedure
+    .input(
+      z.object({
+        id: z.string().uuid(),
+      })
+    )
+    .query(({ ctx, input }) => {
+      return ctx.db.query.meetings.findFirst({
+        where: (meetings, { eq }) => eq(meetings.id, input.id),
+        with: {
+          dates: true,
+        },
+      });
+    }),
+});
