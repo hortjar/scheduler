60748abd54df362df56d6a291eca2a7f2865b4ab
Jaroslav Hort <hortjaroslav0@gmail.com>
2024-03-29 11:50:41 +0100

    feat: get all public meetings

--- src/server/api/routers/meeting.ts
+++ src/server/api/routers/meeting.ts
@@ -1,7 +1,8 @@
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
-import { meetings } from "@/server/db/schema";
+import { meetingDates, meetings } from "@/server/db/schema";
+import { InferInsertModel } from "drizzle-orm";
 
 export const meetingRouter = createTRPCRouter({
   get: publicProcedure
@@ -13,6 +14,28 @@ export const meetingRouter = createTRPCRouter({
     .query(({ ctx, input }) => {
       return ctx.db.query.meetings.findFirst({
         where: (meetings, { eq }) => eq(meetings.id, input.id),
+        with: {
+          dates: true,
+        },
+      });
+    }),
+  getAllPublic: publicProcedure.query(({ ctx }) => {
+    return ctx.db.query.meetings.findMany({
+      where: (meetings, { eq }) => eq(meetings.private, false),
+      with: {
+        dates: true,
+      },
+    });
+  }),
+  getByKey: publicProcedure
+    .input(
+      z.object({
+        key: z.string().length(10),
+      })
+    )
+    .query(({ ctx, input }) => {
+      return ctx.db.query.meetings.findFirst({
+        where: (meetings, { eq }) => eq(meetings.urlKey, input.key),
         with: {
           dates: true,
         },
@@ -32,7 +55,7 @@ export const meetingRouter = createTRPCRouter({
             longitude: z.number(),
           })
           .optional(),
-        dates: z.array(z.date()).optional(),
+        dates: z.array(z.date()),
       })
     )
     .mutation(async ({ ctx, input }) => {
@@ -46,9 +69,21 @@ export const meetingRouter = createTRPCRouter({
           virtual: input.virtual,
         })
         .returning({
+          id: meetings.id,
           creatorKey: meetings.creatorKey,
           urlKey: meetings.urlKey,
         });
+
+      const insertObject = input.dates.map(function (x): InferInsertModel<
+        typeof meetingDates
+      > {
+        return {
+          meetingId: result[0]!.id,
+          date: x,
+        };
+      });
+      await ctx.db.insert(meetingDates).values(insertObject);
+
       return result;
     }),
 });
