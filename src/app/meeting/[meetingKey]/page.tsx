1c992af9e033e153ad9af708739b6cbe62010af0
Jaroslav Hort <hortjaroslav0@gmail.com>
2024-03-29 11:51:52 +0100

    feat: display dates on meeting page

--- src/app/meeting/[meetingKey]/page.tsx
+++ src/app/meeting/[meetingKey]/page.tsx
@@ -1,7 +1,25 @@
+import DateBlock from "@/components/ui/date-block";
+import { H3, H4 } from "@/components/ui/typography";
+import { api } from "@/trpc/server";
+
 export default async function MeetingPage({
   params,
 }: {
   params: { meetingKey: string };
 }) {
-  return <div>Meeting {params.meetingKey}</div>;
+  const meeting = await api.meeting.getByKey.query({
+    key: params.meetingKey,
+  });
+
+  return (
+    <div className="flex flex-col w-full gap-5">
+      <H3>Meeting {meeting?.name}</H3>
+      <H4>Dates</H4>
+      <div className="flex flex-row gap-3 flex-wrap">
+        {meeting?.dates
+          .sort((a, b) => Number(a.date > b.date) - 1)
+          .map((x) => <DateBlock key={"date_" + x.id} date={x.date} />)}
+      </div>
+    </div>
+  );
 }
--- src/components/ui/date-block.tsx
+++ src/components/ui/date-block.tsx
@@ -1,15 +1,45 @@
-import { HTMLAttributes, forwardRef } from "react";
+"use client";
+
+import { HTMLAttributes, forwardRef, useState } from "react";
+import { Card } from "./card";
+import { format } from "date-fns";
+import { H2 } from "./typography";
+import { cn } from "@/lib/utils";
 
 export interface DateBlockProps extends HTMLAttributes<HTMLDivElement> {
   date: Date;
+  onDateClicked?(date: Date, selected: boolean): void;
 }
 
 const DateBlock = forwardRef<HTMLDivElement, DateBlockProps>(
-  ({ date, ...props }, ref) => {
+  ({ onDateClicked: onDateSelected, date, className, ...props }, ref) => {
+    const [isSelected, setIsSelected] = useState<boolean>(false);
+
+    function onDateClick() {
+      if (!onDateSelected) {
+        return;
+      }
+      onDateSelected(date, !isSelected);
+      setIsSelected(!isSelected);
+    }
+
     return (
-      <div ref={ref} {...props}>
-        {date.getMonth()}
-      </div>
+      <Card
+        ref={ref}
+        {...props}
+        className={cn(
+          "flex flex-col gap-2 rounded-3xl p-5 items-center justify-center w-40 h-40 ease-in-out duration-300",
+          className,
+          onDateSelected ? "hover:ring-2 hover:ring-ring cursor-pointer" : "",
+          isSelected ? "ring-2" : ""
+        )}
+        onClick={onDateClick}
+      >
+        <span className="text-sm">{format(date, "eeee")}</span>
+        <H2 className="pb-0">{date.getDay()}</H2>
+        <span className="font-bold">{format(date, "MMMM")}</span>
+        <span className="text-sm">{format(date, "p")}</span>
+      </Card>
     );
   }
 );
