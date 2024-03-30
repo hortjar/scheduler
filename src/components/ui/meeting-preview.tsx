666a71da6d5fab357a248a4113d7b4a1dc2693e6
Jaroslav Hort <hortjaroslav0@gmail.com>
2024-03-30 08:17:19 +0100

    feat: add more info to meeting preview

--- src/components/ui/meeting-preview.tsx
+++ src/components/ui/meeting-preview.tsx
@@ -1,8 +1,14 @@
 import { RouterOutputs } from "@/trpc/shared";
 import { HTMLAttributes, forwardRef } from "react";
-import { Card } from "./card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "./card";
 import Link from "next/link";
-import { H4 } from "./typography";
+import { format } from "date-fns";
 
 export interface MeetingPreviewProps extends HTMLAttributes<HTMLAnchorElement> {
   meeting: RouterOutputs["meeting"]["getAllPublic"][0];
@@ -10,10 +16,26 @@ export interface MeetingPreviewProps extends HTMLAttributes<HTMLAnchorElement> {
 
 const MeetingPreview = forwardRef<HTMLAnchorElement, MeetingPreviewProps>(
   ({ meeting, ...props }, ref) => {
+    function formatMeetingDescription(): string {
+      const location = meeting.virtual ? "A virtual" : "An on location";
+      const dateFormat = "PPPP";
+      let dates = "at an unspecified date";
+      if (meeting.dates.length == 1) {
+        dates = `on ${format(meeting.dates[0]!.date, dateFormat)}`;
+      } else if (meeting.dates.length > 1) {
+        dates = `between ${format(meeting.dates[0]!.date, dateFormat)} and ${format(meeting.dates[meeting.dates.length - 1]!.date, dateFormat)}`;
+      }
+      return `${location} meeting taking place ${dates}`;
+    }
+
     return (
       <Link ref={ref} {...props} href={`/meeting/${meeting.urlKey}`}>
-        <Card className="w-full px-10 py-8 rounded-3xl hover:ring-2 hover:ring-ring ease-in-out duration-300">
-          <H4>{meeting.name}</H4>
+        <Card className="w-full rounded-3xl hover:ring-2 hover:ring-ring ease-in-out duration-300">
+          <CardHeader>
+            <CardTitle>{meeting.name}</CardTitle>
+            <CardDescription>{formatMeetingDescription()}</CardDescription>
+          </CardHeader>
+          <CardContent></CardContent>
         </Card>
       </Link>
     );
