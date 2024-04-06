34ded4c63d8c6a97f0ecb3df0255e4b350e04dbd
Jaroslav Hort <hortjaroslav0@gmail.com>
2024-04-06 17:16:03 +0200

    feat: disable edit button for now

--- src/app/meeting/[meetingKey]/page.tsx
+++ src/app/meeting/[meetingKey]/page.tsx
@@ -36,7 +36,7 @@ export default async function MeetingPage({
           <CardTitle>Meeting {meeting.name}</CardTitle>
           {user.userId && meeting.creator_id == user.userId && (
             <div>
-              <Button variant={"secondary"}>
+              <Button variant={"secondary"} disabled>
                 <Pencil className="w-5 h-5 mr-2" />
                 Edit
               </Button>
