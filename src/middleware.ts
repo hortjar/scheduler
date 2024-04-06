c8e3f4d1177a80c0f3504e3732effaf00f5eae0f
Jaroslav Hort <hortjaroslav0@gmail.com>
2024-04-06 17:16:11 +0200

    feat: add tooltip component

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   publicRoutes: ["/", "/meeting(.*)"],
-  ignoredRoutes: ["(/api/)(.*)", "/(.*)(.js)"],
+  ignoredRoutes: ["(/api/)(.*)", "/(.*)(.js)", "/(.*)(.ico)"],
 });
 
 export const config = {
